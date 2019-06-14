015b84057c2c3405c864fa168b969c8faec6b72d
Marissa Perry <ohthatmarissa@gmail.com>
2019-06-14 15:39:04 -0700

    add nav styling

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
+  appTitle: string = 'MilkBar';
+  
   constructor() { }
 
   ngOnInit() {
