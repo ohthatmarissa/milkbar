bf09b96df5f088e73dfc646527e8e6ee2878be66
Student <example@example.com>
2019-06-20 14:46:01 -0700

    add pipe for classes

--- src/app/class.pipe.ts
+++ src/app/class.pipe.ts
@@ -1,12 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Class } from './class.model';
 
 @Pipe({
   name: 'class'
 })
 export class ClassPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(input: Class[], location: string): Class[] {
+    let output: Class[] = [];
+    if (input){
+    for (let each of input) {
+      if (each.location == location) {
+        output.push(each)
+      }
+    }
+  }
+    return output;
   }
 
 }
